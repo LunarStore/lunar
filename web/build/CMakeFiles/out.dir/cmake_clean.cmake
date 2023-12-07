5c174b69aca9e32b06d22e1a85682a1eeb10e623
LunarStore <1981811204@qq.com>
2023-12-07 21:30:08 +0800

    modify index.html

---
+++ web/build/CMakeFiles/out.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/out.dir/index.cc.o"
+  "/root/dir/Lunar/bin/module/libwebLib.pdb"
+  "/root/dir/Lunar/bin/module/libwebLib.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
